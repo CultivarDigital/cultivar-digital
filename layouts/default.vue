75940279b88e04ab34182b69fa2918b115bf500c
Diego M. Rodrigues <diegomr86@gmail.com>
2021-11-13 23:59:19 -0300

    chore: big bang

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <img
+        :src="require('~/assets/img/cultivar-digital-logo.png')"
+        width="80px"
+      />
+      <span class="ml-3"><strong>Cultivar</strong> digital</span>
+      <v-spacer />
+      <v-btn color="primary" icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <v-footer elevation app class="pa-3">
+      <span class="font-weight-light">
+        Tem uma boa idéia? Vamos desenvolver justos!
+      </span>
+      <v-spacer />
+      <v-btn color="primary" large>Peça um orçamento</v-btn>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex justify-center align-center fill-height">
+  <div class="text-h4 text-sm-h3 text-md-h2 font-weight-light cover">
+      <div class="mb-6"> Design, </div>
+      <div class="mb-6"> Desenvolvimento & </div>
+      <div class="mb-6"> Produção de conteúdo</div>
+    </div>
+</div>
+</template>
+<style lang="sass" scoped>
+  .cover
+    margin: auto
+    width: fit-content
+</style>
