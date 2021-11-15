e930dde8a59ebb5ef06db113c7e9bc87c8efd343
Diego M. Rodrigues <diegomr86@gmail.com>
2021-11-14 23:42:35 -0300

    fix: configuração de deploy no github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/site/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - cultivar-digital',
