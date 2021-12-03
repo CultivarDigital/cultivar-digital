17c856ecee5e31443ee03edfa87b274a0f8e2f03
diegomr86 <diegomr86@users.noreply.github.com>
2021-12-03 00:43:27 +0000

    deploy: cd1c1378491ba5fb56e5b398f5ee1bf88627f45e

--- _nuxt/static/1637027737/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsite\u002F_nuxt\u002Fstatic\u002F1637027737",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fsite\u002F",assetsPath:"\u002Fsite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
