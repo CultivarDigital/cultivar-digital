1862f81ff32a8ee196f4a33f2e9978da08b5b497
diegomr86 <diegomr86@users.noreply.github.com>
2023-02-02 17:44:48 +0000

    deploy: e324581eae23653b31b1dfc00bec6f38e62c6f96

--- _nuxt/d44ae6d.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{547:function(e,n,t){"use strict";t.r(n);var l=t(73),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("teste")])}),[],!1,null,null,null);n.default=component.exports}}]);
