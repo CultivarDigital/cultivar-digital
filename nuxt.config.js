f876192da0f152538e256d27e8205403128f83b6
Diego M. Rodrigues <diegomr86@gmail.com>
2023-02-18 18:39:06 -0300

    fix: 404 error  in dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'server',
+  // target: 'server',
   // ssr: true,
   // generate: {
   //   // fallback: true,
