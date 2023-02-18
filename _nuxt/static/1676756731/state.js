32ef3af40707f6703db705e9b05307b135ed27b6
diegomr86 <diegomr86@users.noreply.github.com>
2023-02-18 21:45:34 +0000

    deploy: 01572bb304ce8e34d8f338501582f9d7bdcff969

---
+++ _nuxt/static/1676756731/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1676756731",layout:"public",error:a,state:{showDrawer:a,customer:a,customerSummary:a,snackbar:{content:b,color:b},auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"","\u002F"));
