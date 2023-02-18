6252bc6abbbc2d8b882f1f6314a14d78fa768709
Diego M. Rodrigues <diegomr86@gmail.com>
2023-02-18 19:55:20 -0300

    fix: 404 error  in dynamic routes

---
+++ layouts/teste.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <Snackbar />
+  </v-app>
+</template>
+
+<script>
+export default {
+  middleware: 'auth',
+}
+</script>
---
+++ pages/teste.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container class="py-6" fluid>
+    Teste
+  </v-container>
+</template>
+<script>
+export default {
+  layout: 'teste',
+}
+</script>
