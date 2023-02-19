2fbeaa7ded2dc543e4e477d2e3a1b0bcd2b9beb4
Diego M. Rodrigues <diegomr86@gmail.com>
2023-02-19 12:21:16 -0300

    feat: melhoria de UI no botão de mudar tema

--- components/SwitchTheme.vue
+++ components/SwitchTheme.vue
@@ -1,20 +1,12 @@
 <template>
   <span>
-    <v-btn v-if="dark" icon @click="switchDarkMode">
-      <v-icon small>mdi-moon-waning-crescent</v-icon>
-    </v-btn>
-    <v-btn v-else icon @click="switchDarkMode">
+    <v-btn icon @click="switchDarkMode">
       <v-icon small>mdi-white-balance-sunny</v-icon>
     </v-btn>
   </span>
 </template>
 <script>
 export default {
-  computed: {
-    dark() {
-      return this.$vuetify.theme.isDark
-    },
-  },
   methods: {
     switchDarkMode() {
       this.$vuetify.theme.isDark = !this.$vuetify.theme.isDark
