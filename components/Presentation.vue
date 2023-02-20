99179ad38a7392c5f845c84a624703c250b9989d
Diego M. Rodrigues <diegomr86@gmail.com>
2023-02-20 14:16:47 -0300

    feat: inicia as mudanças para tornar tudo whitelabel com a lógica de provedores

--- components/Presentation.vue
+++ components/Presentation.vue
@@ -1,10 +1,9 @@
+<!-- eslint-disable vue/no-v-html -->
 <template>
   <section v-if="provider.about" id="presentation" class="py-16 d-flex align-center">
     <v-container class="py-16">
       <h3 class="text-h2 mb-6">Sobre nós</h3>
-      <p>
-        {{ provider.about }}
-      </p>
+      <p v-html="provider.about"></p>
     </v-container>
   </section>
 </template>
