6635a356152b230ccffa4e08967b7f009d463bed
Diego M. Rodrigues <diegomr86@gmail.com>
2024-03-05 15:21:48 -0300

    feat: adiciona política de privacidade

---
+++ pages/politica-de-privacidade.vue
@@ -0,0 +1,452 @@
+<template>
+  <v-container>
+    <TopNavigation active="Política de privacidade" />
+    <div>
+    <h2>Política de Privacidade</h2>
+    <p>
+      Na <em>{{ full_name }}</em
+      >, privacidade e segurança são prioridades e nos comprometemos com a
+      transparência do tratamento de dados pessoais dos nossos usuários. Por
+      isso, esta presente Política de Privacidade estabelece como é feita a
+      coleta, uso e transferência das informações.
+    </p>
+    <p>
+      Ao utilizar nossos serviços, você entende que coletaremos e usaremos suas
+      informações pessoais nas formas descritas nesta Política, sob as normas de
+      Proteção de Dados (LGPD, Lei Federal 13.709/2018), das disposições
+      consumeristas da Lei Federal 8078/1990 e as demais normas do ordenamento
+      jurídico brasileiro aplicáveis.
+    </p>
+    <p>
+      Dessa forma, a <em>{{ full_name }}</em
+      >, doravante denominada simplesmente como "{{ name }}", inscrita no
+      CNPJ/MF sob o nº <em>{{ cnpj }}</em
+      >, no papel de Controladora de Dados, obriga-se ao disposto na presente
+      Política de Privacidade.
+    </p>
+    <h3>1. Quais dados coletamos sobre você e para qual finalidade?</h3>
+    <p>
+      Nosso site coleta e utiliza alguns dados pessoais seus, de forma a
+      viabilizar a prestação de serviços e aprimorar a experiência de uso.
+    </p>
+    <h4>
+      <span>1.1. Dados pessoais fornecidos pelo titular</span>
+    </h4>
+    <ul>
+      <li>
+        &nbsp;<em>Endereço de email:</em> usado apenas para identificar a conta
+        do usuário e comunicações da plataforma (privado)
+      </li>
+      <li>
+        &nbsp;<em>Telefone:</em> usado apenas para identificar a conta
+        do usuário e comunicações da plataforma (privado)
+      </li>
+      <li>
+        &nbsp;<em>Nome completo:</em> usado para a interação do usuário na rede
+        (público)
+      </li>
+    </ul>
+    <!-- <h4>
+        <span
+          >1.2. Dados pessoais coletados automaticamente</
+        >
+      </h4>
+      <ul>
+        <li>&nbsp;<em>Dado e finalidade</em></li>
+        <li>&nbsp;<em>Dado e finalidade</em></li>
+        <li>&nbsp;<em>Dado e finalidade</em></li>
+        <li>&nbsp;<em>Dado e finalidade</em></li>
+      </ul> -->
+    <h3>2. Como coletamos os seus dados?</h3>
+    <p>
+      Nesse sentido, a coleta dos seus dados pessoais ocorre da seguinte forma:
+    </p>
+    <ul>
+      <li>
+        &nbsp;<em>
+          Na tela de cadastro, usuário informa os seus dados, aceita os termos
+          deste documento, os dados são validados e salvos de forma segura em
+          banco de dados.
+        </em>
+      </li>
+    </ul>
+    <h4><span>2.1. Consentimento</span></h4>
+    <p>
+      É a partir do seu consentimento que tratamos os seus dados pessoais. O
+      consentimento é a manifestação livre, informada e inequívoca pela qual
+      você autoriza a <em>{{name}}</em> a tratar seus dados.
+    </p>
+    <p>
+      Assim, em consonância com a Lei Geral de Proteção de Dados, seus dados só
+      serão coletados, tratados e armazenados mediante prévio e expresso
+      consentimento.&nbsp;
+    </p>
+    <p>
+      O seu consentimento será obtido de forma específica para cada finalidade
+      acima descrita, evidenciando o compromisso de transparência e boa-fé do
+      <em>{{ name }}</em> para com seus usuários/clientes, seguindo as
+      regulações legislativas pertinentes.
+    </p>
+    <p>
+      Ao utilizar os serviços do <em>{{ name }}</em> e fornecer seus dados
+      pessoais, você está ciente e consentindo com as disposições desta Política
+      de Privacidade, além de conhecer seus direitos e como exercê-los.
+    </p>
+    <p>
+      A qualquer tempo e sem nenhum custo, você poderá revogar seu
+      consentimento.
+    </p>
+    <p>
+      É importante destacar que a revogação do consentimento para o tratamento
+      dos dados pode implicar a impossibilidade da performance adequada de
+      alguma funcionalidade do site que dependa da operação. Tais consequências
+      serão informadas previamente.
+    </p>
+    <h3>3. Quais são os seus direitos?</h3>
+    <p>
+      o <em>{{ name }}</em> assegura a seus usuários/clientes seus direitos de
+      titular previstos no artigo 18 da Lei Geral de Proteção de Dados. Dessa
+      forma, você pode, de maneira gratuita e a qualquer tempo:
+    </p>
+    <ul>
+      <li>
+        <strong>Confirmar a existência de tratamento de dados</strong>, de
+        maneira simplificada ou em formato claro e completo.
+      </li>
+      <li>
+        <strong>Acessar seus dados</strong>, podendo solicitá-los em uma cópia
+        legível sob forma impressa ou por meio eletrônico, seguro e idôneo.
+      </li>
+      <li>
+        <strong>Corrigir seus dados</strong>, ao solicitar a edição, correção ou
+        atualização destes.
+      </li>
+      <li>
+        <strong>Limitar seus dados</strong> quando desnecessários, excessivos ou
+        tratados em desconformidade com a legislação através da anonimização,
+        bloqueio ou eliminação.
+      </li>
+      <li>
+        <strong>Solicitar a portabilidade de seus dados</strong>, através de um
+        relatório de dados cadastrais que o <em>{{ name }}</em> trata a seu
+        respeito.
+      </li>
+      <li>
+        <strong>Eliminar seus dados</strong> tratados a partir de seu
+        consentimento, exceto nos casos previstos em lei.
+      </li>
+      <li>
+        <strong>Revogar seu consentimento</strong>, desautorizando o tratamento
+        de seus dados.
+      </li>
+      <li>
+        <strong
+          >Informar-se sobre a possibilidade de não fornecer seu
+          consentimento</strong
+        >
+        e sobre as consequências da negativa.
+      </li>
+    </ul>
+    <h3>4. Como você pode exercer seus direitos de titular?</h3>
+    <p>
+      Para exercer seus direitos de titular, você deve entrar em contato com o
+      <em>{{ name }}</em> através dos seguintes meios disponíveis:
+    </p>
+    <ul>
+      <li>
+        &nbsp;Email: <em>{{ email }}</em>
+      </li>
+    </ul>
+    <p>
+      De forma a garantir a sua correta identificação como titular dos dados
+      pessoais objeto da solicitação, é possível que solicitemos documentos ou
+      demais comprovações que possam comprovar sua identidade. Nessa hipótese,
+      você será informado previamente.
+    </p>
+    <h3>5. Como e por quanto tempo seus dados serão armazenados?</h3>
+    <p>
+      Seus dados pessoais coletados pelo
+      <em>{{ name }}</em> serão utilizados e armazenados durante o tempo
+      necessário para a prestação do serviço ou para que as finalidades
+      elencadas na presente Política de Privacidade sejam atingidas,
+      considerando os direitos dos titulares dos dados e dos controladores.
+    </p>
+    <p>
+      De modo geral, seus dados serão mantidos enquanto a relação contratual
+      entre você e o <em>{{ name }}</em> perdurar. Findado o período de
+      armazenamento dos dados pessoais, estes serão excluídos de nossas bases de
+      dados ou anonimizados, ressalvadas as hipóteses legalmente previstas no
+      artigo 16 lei geral de proteção de dados, a saber:
+    </p>
+    <p>I - cumprimento de obrigação legal ou regulatória pelo controlador;</p>
+    <p>
+      II - estudo por órgão de pesquisa, garantida, sempre que possível, a
+      anonimização dos dados pessoais;
+    </p>
+    <p>
+      III - transferência a terceiro, desde que respeitados os requisitos de
+      tratamento de dados dispostos nesta Lei; ou
+    </p>
+    <p>
+      IV - uso exclusivo do controlador, vedado seu acesso por terceiro, e desde
+      que anonimizados os dados.
+    </p>
+    <p>
+      Isto é, informações pessoais sobre você que sejam imprescindíveis para o
+      cumprimento de determinações legais, judiciais e administrativas e/ou para
+      o exercício do direito de defesa em processos judiciais e administrativos
+      serão mantidas, a despeito da exclusão dos demais dados.&nbsp;
+    </p>
+    <p>
+      O armazenamento de dados coletados pelo
+      <em>{{ name }}</em> reflete o nosso compromisso com a segurança e
+      privacidade dos seus dados. Empregamos medidas e soluções técnicas de
+      proteção aptas a garantir a confidencialidade, integridade e
+      inviolabilidade dos seus dados. Além disso, também contamos com medidas de
+      segurança apropriadas aos riscos e com controle de acesso às informações
+      armazenadas.
+    </p>
+    <h3>6. O que fazemos para manter seus dados seguros?</h3>
+    <p>
+      Para mantermos suas informações pessoais seguras, usamos ferramentas
+      físicas, eletrônicas e gerenciais orientadas para a proteção da sua
+      privacidade.
+    </p>
+    <p>
+      Aplicamos essas ferramentas levando em consideração a natureza dos dados
+      pessoais coletados, o contexto e a finalidade do tratamento e os riscos
+      que eventuais violações gerariam para os direitos e liberdades do titular
+      dos dados coletados e tratados.
+    </p>
+    <p>Entre as medidas que adotamos, destacamos as seguintes:</p>
+    <ul>
+      <li>Apenas pessoas autorizadas têm acesso a seus dados pessoais</li>
+      <li>
+        O acesso a seus dados pessoais é feito somente após o compromisso de
+        confidencialidade
+      </li>
+      <li>Seus dados pessoais são armazenados em ambiente seguro e idôneo.</li>
+    </ul>
+    <p>
+      o <em>{{ name }}</em> se compromete a adotar as melhores posturas para
+      evitar incidentes de segurança. Contudo, é necessário destacar que nenhuma
+      página virtual é inteiramente segura e livre de riscos. É possível que,
+      apesar de todos os nossos protocolos de segurança, problemas de culpa
+      exclusivamente de terceiros ocorram, como ataques cibernéticos de
+      <em>hackers</em>, ou também em decorrência da negligência ou imprudência
+      do próprio usuário/cliente.
+    </p>
+    <p>
+      Em caso de incidentes de segurança que possa gerar risco ou dano relevante
+      para você ou qualquer um de nossos usuários/clientes, comunicaremos aos
+      afetados e a Autoridade Nacional de Proteção de Dados sobre o ocorrido, em
+      consonância com as disposições da Lei Geral de Proteção de Dados.
+    </p>
+    <h3>7. Com quem seus dados podem ser compartilhados?</h3>
+    <p>
+      Tendo em vista a preservação de sua privacidade, o
+      <em>{{ name }}</em> não compartilhará seus dados pessoais com nenhum
+      terceiro não autorizado.&nbsp;
+    </p>
+    <p>
+      Existem outras hipóteses em que seus dados poderão ser compartilhados, que
+      são:
+    </p>
+    <p>
+      I - Determinação legal, requerimento, requisição ou ordem judicial, com
+      autoridades judiciais, administrativas ou governamentais competentes.
+    </p>
+    <p>
+      II - Caso de movimentações societárias, como fusão, aquisição e
+      incorporação, de forma automática
+    </p>
+    <p>
+      III - Proteção dos direitos do
+      <em>{{ name }}</em> em qualquer tipo de conflito, inclusive os de teor
+      judicial.
+    </p>
+    <h3>8. Cookies ou dados de navegação</h3>
+    <p>
+      o <em>{{ name }}</em> faz uso de Cookies, que são arquivos de texto
+      enviados pela plataforma ao seu computador e que nele se armazenam, que
+      contém informações relacionadas à navegação do site. Em suma, os Cookies
+      são utilizados para aprimorar a experiência de uso.
+    </p>
+    <p>
+      Ao acessar nosso site e consentir com o este documento, você manifesta
+      conhecer e aceitar a utilização de um sistema de coleta de dados de
+      navegação com o uso de Cookies em seu dispositivo.
+    </p>
+    <p>
+      O <em> {{ name }} </em>utiliza os seguintes Cookies:
+    </p>
+    <ul>
+      <li>
+        Cookies de authenticação que são usados para identificar o usuário
+        logado ao se comunicar com o servidores.
+      </li>
+      <li>
+        Cookies de análise de metadados de navegação que são usados para gerar
+        relatórios de uso e comportamento dos usuários na plataforma
+      </li>
+    </ul>
+    <p>
+      Você pode, a qualquer tempo e sem nenhum custo, alterar as permissões,
+      bloquear ou recusar os Cookies. Todavia, a revogação do consentimento de
+      determinados Cookies pode inviabilizar o funcionamento correto de alguns
+      recursos da plataforma.
+    </p>
+    <p>
+      Para gerenciar os cookies do seu navegador, basta fazê-lo diretamente nas
+      configurações do navegador, na área de gestão de Cookies. Você pode
+      acessar tutoriais sobre o tema diretamente nos links abaixo:
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://support.microsoft.com/pt-br/help/17442/windows-internet-explorer-delete-manage-cookies"
+        rel="noopener"
+        >Internet Explorer</a
+      >.
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://support.mozilla.org/pt-BR/kb/gerencie-configuracoes-de-armazenamento-local-de-s"
+        rel="noopener"
+        >Firefox</a
+      >.
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://support.apple.com/pt-br/guide/safari/sfri11471/mac"
+        rel="noopener"
+        >Safari</a
+      >.
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&amp;oco=1&amp;hl=pt-BR"
+        rel="noopener"
+        >Google Chrome</a
+      >.
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://support.microsoft.com/pt-br/help/4027947/microsoft-edge-delete-cookies"
+        rel="noopener"
+        >Microsoft Edge</a
+      >.
+    </p>
+    <p>
+      Se você usa o
+      <a
+        href="https://help.opera.com/en/latest/web-preferences/#cookies"
+        rel="noopener"
+        >Opera</a
+      >.
+    </p>
+    <h3>9. Alteração desta Política de Privacidade</h3>
+    <p>
+      A atual versão da Política de Privacidade foi formulada e atualizada pela
+      última vez em:
+      <em>{{ last_update }}</em
+      >.
+    </p>
+    <p>
+      Reservamos o direito de modificar essa Política de Privacidade a qualquer
+      tempo, principalmente em função da adequação a eventuais alterações feitas
+      em nosso site ou em âmbito legislativo. Recomendamos que você a revise com
+      frequência.
+    </p>
+    <p>
+      Eventuais alterações entrarão em vigor a partir de sua publicação em nosso
+      site e sempre lhe notificaremos acerca das mudanças ocorridas.
+    </p>
+    <p>
+      Ao utilizar nossos serviços e fornecer seus dados pessoais após tais
+      modificações, você as consente.&nbsp;
+    </p>
+    <h3><span>10. Responsabilidade</span></h3>
+    <p>
+      A <em>{{ full_name }} </em>prevê a responsabilidade dos agentes que atuam
+      nos processos de tratamento de dados, em conformidade com os artigos 42 ao
+      45 da Lei Geral de Proteção de Dados.
+    </p>
+    <p>
+      Nos comprometemos em manter esta Política de Privacidade atualizada,
+      observando suas disposições e zelando por seu cumprimento.
+    </p>
+    <p>
+      Além disso, também assumimos o compromisso de buscar condições técnicas e
+      organizativas seguramente aptas a proteger todo o processo de tratamento
+      de dados.
+    </p>
+    <p>
+      Caso a Autoridade Nacional de Proteção de Dados exija a adoção de
+      providências em relação ao tratamento de dados realizado pela
+      <em>{{ full_name }}, </em>comprometemo-nos a segui-las.&nbsp;
+    </p>
+    <h4>
+      <span>10.1 Isenção de responsabilidade</span>
+    </h4>
+    <p>
+      Conforme mencionado no Tópico 6, embora adotemos elevados padrões de
+      segurança a fim de evitar incidentes, não há nenhuma página virtual
+      inteiramente livre de riscos. Nesse sentido, o
+      <em>{{ name }}</em> não se responsabiliza por:
+    </p>
+    <p>
+      I - Quaisquer consequências decorrentes da negligência, imprudência ou
+      imperícia dos usuários em relação a seus dados individuais. Garantimos e
+      nos responsabilizamos apenas pela segurança dos processos de tratamento de
+      dados e do cumprimento das finalidades descritas no presente instrumento.
+    </p>
+    <p>
+      Destacamos que a responsabilidade em relação à confidencialidade dos dados
+      de acesso é do usuário.
+    </p>
+    <p>
+      II - Ações maliciosas de terceiros, como ataques de
+      <em>hackers</em>, exceto se comprovada conduta culposa ou deliberada da
+      <em>{{ full_name }}.</em>
+    </p>
+    <p>
+      Destacamos que em caso de incidentes de segurança que possam gerar risco
+      ou dano relevante para você ou qualquer um de nossos usuários/clientes,
+      comunicaremos aos afetados e a Autoridade Nacional de Proteção de Dados
+      sobre o ocorrido e cumpriremos as providências necessárias.
+    </p>
+    <p>
+      III - Inveracidade das informações inseridas pelo usuário/cliente nos
+      registros necessários para a utilização dos serviços do
+      <em>{{ name }}</em
+      >; quaisquer consequências decorrentes de informações falsas ou inseridas
+      de má-fé são de inteiramente responsabilidade do usuário/cliente.
+    </p>
+    <h3>11. Encarregado de Proteção de Dados</h3>
+    <p>
+      o <em>{{ name }}</em> disponibiliza os seguintes meios para que você possa
+      entrar em contato conosco para exercer seus direitos de titular:
+      <em>{{ email }}</em>
+    </p>
+  </div>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Cultivar Digital',
+      full_name: 'Cultivar Digital LTDA',
+      cnpj: '47.482.281/0001-20',
+      phone: '(62) 999159008',
+      last_update: '06/09/2022',
+      email: 'atendimento@cultivardigital.com.br',
+    }
+  },
+}
+</script>
